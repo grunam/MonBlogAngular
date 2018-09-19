926cc69cb3593984fc9cbcceb2f1ceee81715c06
grunam <tiabnamik@gmail.com>
2018-09-19 18:49:40 +0100

    Revert "Exercice Mon blog angular V 1.0"
    This reverts commit 1e6a70b2217b6b500677da7cad7f26e7ad11054e.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,66 +1,10 @@
 import { Component } from '@angular/core';
 
-
-
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
-
-
-
 export class AppComponent {
-  title = 'Mon blog';
-  
-  //posts = "coucou"; 
-  
-  /*
-  posts = [
-    {
-      title: 'Mon premier post',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n\
-Nulla sed sollicitudin dolor, ut bibendum augue. Fusce vel facilisis diam. \n\
-Suspendisse potenti. Vivamus porta vitae mauris in feugiat. \n\
-Donec a facilisis lectus. Maecenas felis lectus, pellentesque quis nunc ut, hendrerit pellentesque dui.',
-      loveIts: 1,
-      created_at: new Date(2017, 10, 24, 11)
-    }
-  ];
-*/
-  
- 
-  posts = [
-    {
-      title: 'Mon premier post',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n\
-Nulla sed sollicitudin dolor, ut bibendum augue. Fusce vel facilisis diam. \n\
-Suspendisse potenti. Vivamus porta vitae mauris in feugiat. \n\
-Donec a facilisis lectus. Maecenas felis lectus, pellentesque quis nunc ut, hendrerit pellentesque dui.',
-      loveIts: 1,
-      created_at: new Date(2017, 10, 24, 11)
-    },
-    {
-      title: 'Mon deuxième post',
-      content: 'Duis id ante dapibus, maximus arcu at, tristique ex. \n\
-Mauris laoreet libero a tincidunt semper. Integer sit amet ultrices felis, condimentum porta lorem. \n\
-Donec sollicitudin vel velit eu scelerisque. \n\
-In blandit porttitor mauris, ut dictum risus elementum et. Curabitur quis bibendum nulla.',
-      loveIts: 0,
-      created_at: new Date(2017, 10, 24, 11)
-    },
-    {
-      title: 'Encore un post',
-      content: 'Sed eu turpis nisi. Sed sollicitudin finibus purus, at ornare nunc lacinia vel. \n\
-Suspendisse volutpat tempor ex a commodo. \n\
-Etiam aliquet varius diam, ut accumsan elit accumsan sit amet. \n\
-Pellentesque et ullamcorper turpis. Fusce vitae felis nisi. Vivamus sed ipsum lectus. \n\
-Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      created_at: new Date(2017, 10, 24, 11)
-    }
-  ];
-
+  title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PostListComponentComponent } from './post-list-component/post-list-component.component';
-import { PostListItemComponentComponent } from './post-list-item-component/post-list-item-component.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PostListComponentComponent,
-    PostListItemComponentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
